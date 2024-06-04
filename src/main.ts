2101ea14681db38bdb32f46ec562f82b7dfdba8d
caiodonat <cdonat.dev@gmail.com>
2024-06-03 22:41:39 -0300

    build: nestjs init

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+
+  const APP_PORT = process.env.APP_PORT || 3333;
+
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+
+
+  await app.listen(APP_PORT);
+}
+bootstrap();
